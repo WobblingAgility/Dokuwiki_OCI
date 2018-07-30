050797cf6ac63b697e7b6d6b0c677e316f583b3e
root <root@FED-ITO-LT01.bcdmetal.local>
2018-07-30 16:38:06 -0400

    Started configuring lighttpd

--- dokuwiki_buildah.sh
+++ dokuwiki_buildah.sh
@@ -10,9 +10,9 @@ scratchmnt=$(buildah mount $newcontainer)
 dnf install\
        	bash\
 	coreutils\
-	tar\
-	wget\
-	nginx\
+	lighttpd\
+	php-cli\
+	lighttpd-fastcgi\
 	php-fpm\
        	php-ldap\
 	php-xml\
@@ -29,13 +29,19 @@ dnf clean all -y --installroot $scratchmnt --releasever 7
 rm -rf $scratchmnt/var/cache/yum
 
 # install/configure dokuwiki
+sed -i s/apache/lighttpd/g $scratchmnt/etc/php-fpm.d/www.conf
+sed -i s/"#include \"conf.d\/fastcgi.conf\""/"include \"conf.d\/fastcgi.conf\""/ $scratchmnt/etc/lighttpd/modules.conf
+echo 'include "/etc/lighttpd/vhosts.d/*.conf"' >> $scratchmnt/etc/lighttpd/lighttpd.conf
 wget -p -O $scratchmnt/dokuwiki.tgz "https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz"
 mkdir $scratchmnt/dokuwiki
 tar -zxf $scratchmnt/dokuwiki.tgz -C $scratchmnt/dokuwiki --strip-components 1
-buildah run $newcontainer chown -R nginx:nginx /dokuwiki
+buildah run $newcontainer chown -R lighttpd:lighttpd /dokuwiki
+buildah add $newcontainer lighttpd-dokuwiki.conf /etc/lighttpd/vhosts.d/dokuwiki.conf
 
 # configure container
 buildah config --label name=$name $newcontainer
+buildah config --cmd "/usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf" $newcontainer
+buildah config --port 80 --port 443 $newcontainer
 
 # commit the image
 buildah unmount $newcontainer
